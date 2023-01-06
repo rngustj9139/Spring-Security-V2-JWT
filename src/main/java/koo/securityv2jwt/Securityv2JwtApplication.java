bef2e7e3cf2b560835d78bc3285dce325ec89004
rngustj9139 <mnb9139@khu.ac.kr>
2023-01-06 19:41:16 +0900

    Setting first environment

---
+++ src/main/java/koo/securityv2jwt/Securityv2JwtApplication.java
@@ -0,0 +1,13 @@
+package koo.securityv2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securityv2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Securityv2JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koo/securityv2jwt/Securityv2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.securityv2jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securityv2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
