1315e87cd3d0f4d92d0a42a7fb78aed58dde6eea
rngustj9139 <mnb9139@khu.ac.kr>
2023-01-08 03:31:04 +0900

    Add RestApiController, User

---
+++ src/main/java/koo/securityv2jwt/model/User.java
@@ -0,0 +1,19 @@
+package koo.securityv2jwt.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+    private String password;
+    private String roles;
+
+}
