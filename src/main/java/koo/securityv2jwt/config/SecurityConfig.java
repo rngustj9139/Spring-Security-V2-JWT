ec90c240beb06b77dc43b4bc6d1588f2b7a5c367
rngustj9139 <mnb9139@khu.ac.kr>
2023-07-12 11:50:43 +0900

    Refactor: SecurityConfig

--- src/main/java/koo/securityv2jwt/config/SecurityConfig.java
+++ src/main/java/koo/securityv2jwt/config/SecurityConfig.java
@@ -20,9 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다.
             .and()
-        .addFilter(corsFilter) // 필터 등록
-        .formLogin().disable() // jwt 서버를 쓰니깐 폼로그인 disable
-        .httpBasic().disable() // 기본적인 http 로그인 방식 사용 안함
+        .addFilter(corsFilter) // 필터 등록 (인증이 필요하면 이 필터를 이용하고 인증이 필요 없으면 컨트롤러에 @CorsOrigin 어노테이션 붙이기)
+        .formLogin().disable() // jwt 서버를 쓰니깐 폼로그인 disable (폼 태그써서 로그인 안할 것이다.)
+        .httpBasic().disable() // 기본적인 http 로그인 방식 사용 안함 (매번 헤더의 authorization에 id와 password를 담아 전송하는 방식 이거 쓰면 쿠키와 세션을 이용할 필요가 없음, 이것의 단점은 암호화가 안되는 것이고 암호화를 하려면 https를 이용해야함)
         .authorizeRequests()
         .antMatchers("/api/v1/user/**")
         .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
