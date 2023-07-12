c3178aff9f9c520682e8691058a82a5861e9891a
rngustj9139 <mnb9139@khu.ac.kr>
2023-07-12 15:07:30 +0900

    Refactor: MyFilter1

--- src/main/java/koo/securityv2jwt/filter/MyFilter1.java
+++ src/main/java/koo/securityv2jwt/filter/MyFilter1.java
@@ -6,6 +6,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 public class MyFilter1 implements Filter {
@@ -18,11 +19,22 @@ public class MyFilter1 implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
 
         if (req.getMethod().equals("POST")) {
+            /**
+             * localhost:8080/token에 POST 요청하기
+             * 포스트맨 headers에 Authorization에 코스라고 넣어보기
+             * Authorization이 코스가 아니면 컨트롤러 진입 불가능
+             */
+            log.info("POST 요청됨");
             String headerAuth = req.getHeader("Authorization");
             log.info("headerAuth: {}", headerAuth);
-        }
 
-        chain.doFilter(req, res);
+            if (headerAuth.equals("코스")) {
+                chain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안됨");
+            }
+        }
     }
 
 }
