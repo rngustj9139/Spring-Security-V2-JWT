82556718fded66437414016c56e8aa5906c99d6e
rngustj9139 <mnb9139@khu.ac.kr>
2023-07-13 15:21:43 +0900

    Refactor: JwtAuthenticationFilter

--- src/main/java/koo/securityv2jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/koo/securityv2jwt/config/jwt/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 
 /**
  * 스프링 시큐리티는 UsernamePasswordAuthenticationFilter라는 필터를 가지고 있음
@@ -17,6 +18,9 @@ import javax.servlet.http.HttpServletResponse;
  * 근데 formLigin().disable()이므로 이 필터가 동작을 안하는데
  * SecurityConfig에 등록을 하면된다.
  * 이 필터는 AuthenticationManager로 로그인을 진행시킴
+ * AuthenticationManager가 실행되면 PrincipalDetailsService의 loadUserByUsername이 실행된다
+ * 이후 PrincipalDetails를 세션에 담고(굳이 세션에 담는 이유: SecurityConfig의 인가 관리를 위해서이다.)
+ * jwt를 만들어서 응답한다.
  */
 @RequiredArgsConstructor
 @Slf4j
@@ -27,6 +31,19 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override // 로그인 시도하고 필터 동작시 이 함수가 실행된다.
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         log.info("JwtAuthenticationFilter: 로그인 시도중");
+
+        try {
+            log.info("==================");
+//          log.info(request.getInputStream().toString());
+            BufferedReader br = request.getReader();
+            String input = null;
+
+            while ((input = br.readLine()) != null) {
+                log.info("input: {}", input); // 로그인시 username=ssar&assword=1234가 출력됨 json으로 보내면 json형태로 출력된다.
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return super.attemptAuthentication(request, response);
     }
